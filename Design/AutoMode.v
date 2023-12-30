fc94cec9a945bd09f09e2a959610b487d0aa67b8
nguyuyen <nguyuyennguyen@gmail.com>
2023-12-30 20:03:30 +0700

    change the way to counter

--- Design/AutoMode.v
+++ Design/AutoMode.v
@@ -28,44 +28,48 @@ module autoMode(
         if (enable == 1) begin
             case (state)
                 GR: begin
-                    if (timeLane1 == 1) begin
+                    if (timeLane1 == 0) begin
                         state <= YR;
-                        timeLane1 <= yellowTime;
+                        timeLane1 <= yellowTime - 1;
                     end else begin
                         timeLane1 <= timeLane1 - 1;
                     end 
                     timeLane2 <= timeLane2 - 1;
                 end
                 YR: begin
-                    if (timeLane1 == 1) begin
+                    if (timeLane1 == 0) begin
                         state <= RG;
-                        timeLane2 <= greenTime;
-                        timeLane1 <= redTime;
+                        timeLane2 <= greenTime - 1;
+                        timeLane1 <= redTime - 1;
                     end else begin
                         timeLane1 <= timeLane1 - 1;
                         timeLane2 <= timeLane2 - 1;
                     end
                 end
                 RG: begin
-                    if (timeLane2 == 1) begin
+                    if (timeLane2 == 0) begin
                         state <= RY;
-                        timeLane2 <= yellowTime;
+                        timeLane2 <= yellowTime - 1;
                     end else begin
                         timeLane2 <= timeLane2 - 1;                    
                     end 
                     timeLane1 <= timeLane1 - 1;
                 end
                 RY: begin
-                    if (timeLane2 == 1) begin
+                    if (timeLane2 == 0) begin
                         state <= GR;
-                        timeLane1 <= greenTime;
-                        timeLane2 <= redTime;
+                        timeLane1 <= greenTime - 1;
+                        timeLane2 <= redTime - 1;
                     end else begin
                         timeLane2 <= timeLane2 - 1; 
                         timeLane1 <= timeLane1 - 1;
                     end
                 end
-                default: state <= GR;
+                default: begin
+                    state <= GR;
+                    timeLane1 <= greenTime - 1;
+                    timeLane2 <= redTime - 1;
+                end
             endcase
         end
     end
